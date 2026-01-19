fab7b6a3c593b37f2c35cdd3f0d599b3b2fd222e
Arthur Wagenknecht <oarthurdev@gmail.com>
2026-01-19 10:43:37 -0300

    feat(repoai): Crie mais um menu chamado "Soluções", após o menu de Serviços, essa página será o portfolio de soluções que a Vortex Hub vende, por enquanto só terá 1, que é o app AgendaFacil(http://agendafacil.vortexhub.com.br/) - home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 import Hero from "@/components/hero";
 import About from "@/components/about";
 import Services from "@/components/services";
+import Solutions from "@/components/solutions";
 import Benefits from "@/components/benefits";
 import Team from "@/components/team";
 import Contact from "@/components/contact";
@@ -14,6 +15,7 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <Solutions />
       <Benefits />
       <Team />
       <Contact />
